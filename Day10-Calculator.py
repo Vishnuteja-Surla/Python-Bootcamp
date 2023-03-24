04eacea91fbb6b4e68962ac13b62330bfe4d9bc1
Vishnuteja-Surla <cs21b2037@iiitdm.ac.in>
2023-03-24 07:44:24 +0530

    Complete Day10 Calculator Project

---
+++ Day10-Calculator.py
@@ -0,0 +1,58 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("Enter the first number : "))
+    continue_loop = True
+    while continue_loop:
+        for i in operations:
+            print(i)
+        op = input("Enter the operation you want to perform from above : ")
+        num2 = float(input("Enter the second number : "))
+        result = operations[op](num1, num2)
+        print(f"{num1} {op} {num2} = {result}")
+        exit = input("Type 'y' continue with the result or 'n' to start fresh or 'e' to exit: ")
+        if exit == 'n':
+            continue_loop = False
+            calculator()
+        elif exit == 'e':
+            return
+        else:
+            num1 = result
+
+calculator()
